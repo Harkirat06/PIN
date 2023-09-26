9898fd9c7f727ac8a9f0e5d91ffbd166c1378bd4
Harkirat Singh Kaur <khalsaharkirat97@gmail.com>
2023-09-26 11:58:54 +0200

    Conectada la base de datos

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
+require("./mongoose")
+require('dotenv').config()
 const express = require("express");
 const path = require('path');
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 

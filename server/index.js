6c487b0d60fc0f0fac93eec11fa0fa1a37b9daf0
Harkirat Singh Kaur <khalsaharkirat97@gmail.com>
2023-09-26 12:06:01 +0200

    Añadido otra dependencia

--- server/index.js
+++ server/index.js
@@ -1,23 +1,25 @@
 require("./mongoose")
 require('dotenv').config()
-const express = require("express");
-const path = require('path');
+const express = require("express")
+const path = require('path')
+const compression = require('compression')
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 
-const app = express();
+const app = express()
+app.use(compression())
 
-app.use(express.static(path.resolve(__dirname, '../client/build')));
+app.use(express.static(path.resolve(__dirname, '../client/build')))
 
 
 app.get("/api", (req, res) => {
-  res.json({ message: "Proyecto inicial de PIN!" });
-});
+  res.json({ message: "Proyecto inicial de PIN!" })
+})
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-});
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'))
+})
 
 app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
-});
+  console.log(`Server listening on ${PORT}`)
+})

f799612454d005234b90b4c47d5cb237deaaacf4
Harkirat <khalsaharkirat97@gmail.com>
2023-10-09 17:07:21 +0200

    Añadidos comentarios explicativos

--- server/index.js
+++ server/index.js
@@ -1,10 +1,9 @@
-require("./mongoose")
-require('dotenv').config()
-const express = require("express")
-const path = require('path')
-const compression = require('compression')
-const cors = require('cors')
-const pcBuilderRouter = require('./controllers/pcbuilder')
+require("./mongoose") // Base de datos
+require('dotenv').config() // Utilizar variables del archivo .env
+const express = require("express") // Servidor
+const compression = require('compression') // Rendimiento
+const cors = require('cors') // Control de acceso
+const pcBuilderRouter = require('./controllers/pcbuilder') // Gestion de pc Builder
 const PORT = process.env.PORT || 5000
 
 const server = express()
@@ -13,12 +12,13 @@ server.use(compression())
 server.use(cors())
 
 server.use("/api/pcbuilder", pcBuilderRouter)
+
 server.get("/api", (req, res) => {
   res.json({ message: "Proyecto inicial de PIN!" })
 })
 
 server.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'))
+  res.json({ message: "Un saludo desde el servidor!" })
 })
 
 server.listen(PORT, () => {

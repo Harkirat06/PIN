764060982d82e2750f96b5c556d0345d1b3e812a
Pablo José Peral Tamarit <PJPERTAM@upv.edu.es>
2023-10-11 09:04:14 +0200

    informacion añadida de builds

--- server/controllers/pcBuilder.js
+++ server/controllers/pcBuilder.js
@@ -1,11 +1,11 @@
 const pcBuilderRouter = require("express").Router()
 
-const GAMABAJA = 1;
-const GAMABAJAMEDIA = 2;
-const GAMAMEDIA = 3;
-const GAMAMEDIAALTA = 4;
-const GAMAALTA = 5;
-const GAMAPREMIUM = 6;
+const gamaBaja = 1;
+const gamaBajaMedia = 2;
+const gamaMedia = 3;
+const gamaMediaAlta = 4;
+const gamaAlta = 5;
+const gamaPremium = 6;
 
 const configuration = {
     "placaBase": {
@@ -13,49 +13,57 @@ const configuration = {
         "nombre": "ASUS PRIME Z790-P WIFI",
         "tamaño": "ATX",
         "socket": "Intel LGA 1700",
-        "gama": GAMAALTA
+        "gama": gamaAlta
     },
     "CPU": {
         "type": "CPU",
         "marca": "Intel",
         "nombre": "Intel Core i7-13700K 3.4 GHz",
-        "gama": GAMAALTA,
+        "gama": gamaAlta,
         "socket": "Intel LGA 1700",
-        "gpu": true,
+        "gpu": true
     },
     "ram": {
         "marca": "Corsair",
         "nombre":"Corsair Vengeance RGB DDR5 6000MHz PC5-48000 32GB 2x16GB CL36 Negra",
         "tipo": "DDR5",
-        "capacidad": "32",
-        "gama": GAMAMEDIA,
+        "capacidad": 32,
+        "gama": gamaMedia
     },
     "disco": {
         "marca": "Samsung",
         "nombre": "Samsung 980 Pro SSD 1TB PCIe NVMe M.2",
-        "capacidad": "1", //tb
+        "capacidad": 1,
         "tecnologia": "m.2",
-        "gama": GAMAALTA,
+        "gama": gamaAlta
     },
     "GPU": {
         "marca": "Gigabyte",
         "nombre":"Gigabyte GeForce RTX 4070 Windforce OC 12 GB GDDR6X DLSS3",
-        "gama": GAMAMEDIAALTA,
+        "gama": gamaMediaAlta
     },
-    "fuenteAlimentacion": {},
+    "fuenteAlimentacion": {
+        "marca": "Forgeon",
+        "nombre": "Forgeon Bolt PSU 850W 80+ Gold Full Modular Fuente de Alimentación",
+        "potencia": "850",
+        "ochetaplus": true,
+        "gama": gamaAlta
+    },
     "monitor": {
         "marca": "ASUS",
         "nombre": "ASUS TUF Gaming VG249Q1A 23.8 LED IPS FullHD 165Hz FreeSync Premium",
-        "gama": GAMAMEDIA,
+        "gama": gamaMedia
     },
-    "raton": {},
-    "teclado": {},
-    "caja": {},
+    "caja": {
+        "marca": "Tempest",
+        "nombre": "Tempest Umbra RGB Torre ATX Blanca",
+        "gama": gamaAlta
+    },
     "disipador": {
         "marca": "Forgeon",
         "nombre": "Forgeon Azoth 240 ARGB Kit de Refrigeración Líquida 240mm Negro",
         "tipo": "Liquida",
-        "gama": GAMAMEDIA,
+        "gama": gamaMedia
     },
 }
 
@@ -63,23 +71,23 @@ const cpuList = [
     {
         "marca": "Intel",
         "nombre": "Intel Core i7-13700K 3.4 GHz",
-        "gama": GAMAALTA,
+        "gama": gamaAlta,
         "socket": "Intel LGA 1700",
-        "gpu": true,
+        "gpu": true
     },
     {
         "marca": "Intel",
         "nombre": "Intel Core i5-13600KF 3.5 GHz",
-        "gama": GAMAMEDIA,
+        "gama": gamaMedia,
         "socket": "Intel LGA 1700",
-        "gpu": false,
+        "gpu": false
     },
     {
         "marca": "Intel",
         "nombre": "Intel Core i3-13100 3.4 GHz/4.5 GHz",
-        "gama": GAMABAJA,
+        "gama": gamaBaja,
         "socket": "Intel LGA 1700",
-        "gpu": true,
+        "gpu": true
     },
 ]
 
@@ -90,7 +98,7 @@ const placasList = [
         "tamaño": "ATX",
         "socket": "Intel LGA 1700",
         "tipoRam": "DDR5",
-        "gama": GAMAALTA,
+        "gama": gamaAlta
     },
     {
         "marca": "Asus",
@@ -98,7 +106,7 @@ const placasList = [
         "tamaño": "ATX",
         "socket": "Intel LGA 1700",
         "tipoRam": "DDR5",
-        "gama": GAMAMEDIA,
+        "gama": gamaMedia
     },
     {
         "marca": "MSI",
@@ -106,7 +114,7 @@ const placasList = [
         "tamaño": "ATX",
         "socket": "Intel LGA 1200",
         "tipoRam": "DDR5",
-        "gama": GAMABAJA,
+        "gama": gamaBaja
     },
 ] 
 
@@ -114,17 +122,17 @@ const gpuList = [
     {
         "marca": "MSI",
         "nombre":"MSI GeForce RTX 3060 VENTUS 2X OC LHR 12GB GDDR6",
-        "gama": GAMAMEDIA,
+        "gama": gamaMedia
     },
     {
         "marca": "AMD",
         "nombre":"Sapphire Pulse AMD Radeon RX 6700 XT 12GB GDDR6",
-        "gama": GAMAMEDIA,
+        "gama": gamaMedia
     },
     {
         "marca": "Gigabyte",
         "nombre":"Gigabyte GeForce RTX 4070 Windforce OC 12 GB GDDR6X DLSS3",
-        "gama": GAMAMEDIAALTA,
+        "gama": gamaMediaAlta
     },
 ]
 
@@ -134,21 +142,21 @@ const ramList = [
         "nombre":"Corsair Vengeance LPX DDR4 3200MHz PC4-25600 32GB 2x16GB CL16",
         "tipo": "DDR4",
         "capacidad": 32, //gb
-        "gama": GAMAMEDIA,
+        "gama": gamaMedia
     },
     {
         "marca": "Kingston",
         "nombre":"Kingston FURY Beast DDR4 3200 MHz 16GB 2x8GB CL16",
         "tipo": "DDR4",
         "capacidad": 16,
-        "gama": GAMABAJAMEDIA,
+        "gama": gamaBajaMedia
     },
     {
         "marca": "Corsair",
         "nombre":"Corsair Vengeance RGB DDR5 6000MHz PC5-48000 32GB 2x16GB CL36 Negra",
         "tipo": "DDR5",
         "capacidad": 32,
-        "gama": GAMAMEDIA,
+        "gama": gamaMedia
     },
 ]
 
@@ -158,21 +166,21 @@ const discoList = [
         "nombre": "Samsung 870 QVO SSD 1TB SATA3",
         "capacidad": 1, //tb
         "tecnologia": "Sata",
-        "gama": GAMAMEDIA,
+        "gama": gamaMedia
     },
     {
         "marca": "Samsung",
         "nombre": "Samsung 980 Pro SSD 1TB PCIe NVMe M.2",
         "capacidad": 1,
         "tecnologia": "m.2",
-        "gama": GAMAALTA,
+        "gama": gamaAlta
     },
     {
         "marca": "Seagate",
         "nombre": "Seagate BarraCuda 3.5 2TB SATA 3",
         "capacidad": 2,
         "tecnologia": "Sata",
-        "gama": GAMABAJA,
+        "gama": gamaBaja
     },
 ]
 
@@ -180,17 +188,17 @@ const monitorList = [
     {
         "marca": "ASUS",
         "nombre": "ASUS TUF Gaming VG249Q1A 23.8 LED IPS FullHD 165Hz FreeSync Premium",
-        "gama": GAMAMEDIA,
+        "gama": gamaMedia
     },
     {
         "marca": "AOC",
         "nombre": "AOC 24B2XHM2 23.8 LED FullHD",
-        "gama": GAMABAJA,
+        "gama": gamaBaja
     },
     {
         "marca": "LG",
         "nombre": "LG 27GR95QE-B 26.5 OLED QHD 240Hz G-Sync/FreeSync Premium",
-        "gama": GAMAPREMIUM,
+        "gama": gamaPremium
     },
 ]
 
@@ -199,19 +207,61 @@ const disipadorList = [
         "marca": "Tempest",
         "nombre": "Tempest Cooler 4Pipes 120mm Ventilador CPU Negro",
         "tipo": "Ventilador",
-        "gama": GAMABAJA,
+        "gama": gamaBaja
     },
     {
         "marca": "Forgeon",
         "nombre": "Forgeon Azoth 240 ARGB Kit de Refrigeración Líquida 240mm Negro",
         "tipo": "Liquida",
-        "gama": GAMAMEDIA,
+        "gama": gamaMedia
     },
     {
         "marca": "Forgeon",
         "nombre": "Forgeon Azoth 360 ARGB Kit de Refrigeración Líquida 360mm Negro",
         "tipo": "Liquida",
-        "gama": GAMAALTA,
+        "gama": gamaAlta
+    },
+]
+
+const cajaList = [
+    {
+        "marca": "Forgeon",
+        "nombre": "Forgeon Mithril ARGB Mesh Torre ATX Negra",
+        "gama": gamaMedia
+    },
+    {
+        "marca": "Tempest",
+        "nombre": "Tempest Umbra RGB Torre ATX Blanca",
+        "gama": gamaAlta
+    },
+    {
+        "marca": "Nox",
+        "nombre": "Nox Hummer Void USB 3.0 Negro",
+        "gama": gamaBaja
+    },
+]
+
+const fuenteList = [
+    {
+        "marca": "Tempest",
+        "nombre": "Tempest PSU PRO 650W 80+ Bronze Fuente de Alimentación",
+        "potencia": "650", //W
+        "ochetaplus": true,
+        "gama": gamaMedia
+    },
+    {
+        "marca": "Forgeon",
+        "nombre": "Forgeon Bolt PSU 850W 80+ Gold Full Modular Fuente de Alimentación",
+        "potencia": "850",
+        "ochetaplus": true,
+        "gama": gamaAlta
+    },
+    {
+        "marca": "Tempest",
+        "nombre": "Tempest PSU PRO 650W 80+ Bronze Fuente de Alimentación",
+        "potencia": "750",
+        "ochetaplus": false,
+        "gama": gamaBaja
     },
 ]
 

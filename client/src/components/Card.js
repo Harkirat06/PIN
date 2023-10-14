8c79b965f90e2a561e5b2ebd4685e3cc8416600d
Adrián González Recaséns <AGONREC@upv.edu.es>
2023-10-14 10:57:56 +0200

    Comprobacion precios y creacion Searcher&card

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -8,9 +8,15 @@ function Card ({context}) {
         <div className="Card">
             <img src={imagen} alt={nombre}/>
             <h3>{nombre}</h3>
+            if (precioAmazon != null){
             <p> PrecioAmazon: ${precioAmazon} </p>
+}
+            if (precioEbay != null) {
             <p> PrecioEbay: ${precioEbay} </p>
+}
+            if (precioSegundaMano != null){
             <p> PrecioSegundaMano: ${precioSegundaMano} </p>
+}
         </div>
     )
 

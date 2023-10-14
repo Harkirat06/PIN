8c0751244a262b4319c0cb1ba5ea847c0c70ecf1
vicentsasi <96072949+vicentsasi@users.noreply.github.com>
2023-10-14 16:38:31 +0200

    ejemplo para el componente card

--- client/src/components/Cards.js
+++ client/src/components/Cards.js
@@ -5,8 +5,40 @@ import React from "react";
 import Card from "./Card";
 
 function Cards({ context }) {
-    const { archivos, setRefrescar, setPath, path, setArchivos, refrescar, token,setToken,
-        error, setError, finish, setFinish, showUpload, setShowUpload,
-        showDownload, setShowDownload, setShowVideo, showVideo } = useContext(context)
-    const navigate = useNavigate()
-}
+    const ejemplo = [
+        {
+          nombre: "Producto 1",
+          imagen: "url_imagen_1.jpg",
+          precioAmazon: 100,
+          precioEbay: 90,
+          precioSegundaMano: 80,
+        },
+        {
+          nombre: "Producto 2",
+          imagen: "url_imagen_2.jpg",
+          precioAmazon: 120,
+          precioEbay: 110,
+          precioSegundaMano: 100,
+        },
+        // Agrega más cartas manualmente aquí
+      ];
+
+      return (
+        <div className="container">
+          <div className="row">
+            {ejemplo.map((cardData, index) => (
+              <div className="col-12 col-md-6 col-lg-4" key={index}>
+                <Card
+                  nombre={cardData.nombre}
+                  imagen={cardData.imagen}
+                  precioAmazon={cardData.precioAmazon}
+                  precioEbay={cardData.precioEbay}
+                  precioSegundaMano={cardData.precioSegundaMano}
+                  context={context}
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+      );
+    }

38b12f6d252cc63893d7554f3c8cd7ab2586d3b5
Harkirat <khalsaharkirat97@gmail.com>
2023-10-14 21:53:50 +0200

    axios funciones utiles

---
+++ client/src/components/Axios.js
@@ -0,0 +1,50 @@
+import axios from 'axios'
+
+export async function getImage(nombre){
+    const response = await axios.get("/image/" + nombre)
+    .then((response) => response.blob())
+      .then((imageBlob) => {
+        const imageUrl = URL.createObjectURL(imageBlob);
+        return imageUrl
+      });
+      return response
+}
+
+export async function deleteFiles(path, token) {
+    return axios.delete("/delete", {
+        headers: {"Authorization" : `Bearer ${token}`},
+        params: {
+            path: path
+        }
+    })
+}
+
+export async function registerUser(user) {
+    const res = await axios.post("/users", user).catch((e) => {
+        const { response } = e
+        return response
+    })
+    return res
+}
+
+export async function registerImage(image) {
+    console.log(image)
+    const formData = new FormData();
+    formData.append("docs", image);
+    const res = await axios.post("/image", formData).catch((e) => {
+        const { response } = e
+        return response
+    })
+    console.log(res)
+    return res
+}
+
+export async function loginUser(user) {
+    const res = await axios.post("/login", user).catch((e) => {
+        const { response } = e
+        return response
+    })
+    return res
+}
+
+

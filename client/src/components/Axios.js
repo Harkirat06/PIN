493622847e20b7bdfb93532e884fbfee2c81b38b
Adrián González Recaséns <AGONREC@upv.edu.es>
2023-10-27 09:10:16 +0200

    Cambio axios para las llamadas al servidor

--- client/src/components/Axios.js
+++ client/src/components/Axios.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 
 export async function buildPorPrecio(precio){
-    const response = await axios.get("/api/pcbuilder/:precio",{
+    const response = await axios.get("/api/pcbuilder/precio",{
         params: {
             precio: precio
         }
@@ -12,7 +12,7 @@ export async function buildPorPrecio(precio){
 
 }
 export async function buildPorGama(gama){
-        const response = await axios.get("/api/pcbuilder/:gama",{
+        const response = await axios.get("/api/pcbuilder/gama",{
             params: {
                 gama: gama
             }

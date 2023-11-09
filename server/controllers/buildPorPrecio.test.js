4623bd01010db3d435f9d410e23d6e7010de2d55
Pablo José Peral Tamarit <PJPERTAM@upv.edu.es>
2023-11-09 12:21:32 +0100

    test WIP

--- server/controllers/buildPorPrecio.test.js
+++ server/controllers/buildPorPrecio.test.js
@@ -25,6 +25,34 @@ describe("buildPorPrecio function", () => {
 
   const presupuestoSuficiente = 1500;
 
+  describe('buildPorPrecio', () => {
+    it('Debería devolver un error si el presupuesto es 0 o negativo', () => {
+      const result = buildPorPrecio(-100, false);
+      expect(result).toEqual({ "Error": "El presupuesto es 0 o negativo" });
+    });
+  
+    it('Debería devolver un error si no se puede encontrar una build por el presupuesto especificado', () => {
+      const result = buildPorPrecio(100, false);
+      expect(result).toEqual({ "Error": "No se ha podido encontrar una build por el presupuesto especificado" });
+    });
+  
+    it('Debería devolver una configuración por precio válida', () => {
+      const result = buildPorPrecio(2000, false);
+      expect(result).toHaveProperty('placaBase');
+      expect(result).toHaveProperty('cpu');
+      expect(result).toHaveProperty('ram');
+      expect(result).toHaveProperty('gpu');
+      expect(result).toHaveProperty('disiapdor');
+      expect(result).toHaveProperty('fuente');
+      expect(result).toHaveProperty('m2');
+      expect(result).toHaveProperty('sata');
+      expect(result).toHaveProperty('caja');
+      expect(result).toHaveProperty('monitor');
+      expect(result).toHaveProperty('raton');
+      expect(result).toHaveProperty('teclado');
+    });
+  });
+
   jest.mock("./shortearListasPorPrecio", () => {
     return jest.fn((gama) => {
       return expectedConfiguration;

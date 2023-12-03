cc27d80767a1dbae23773815a2bb95050cc687f9
Harkirat <khalsaharkirat97@gmail.com>
2023-12-03 20:54:12 +0100

    subir archivo

--- client/src/components/PcBuilder.test.js
+++ client/src/components/PcBuilder.test.js
@@ -1,6 +1,6 @@
 import React from "react"
 import "@testing-library/jest-dom"
-import {prettyDOM, render} from "@testing-library/react"
+import {prettyDOM, render, fireEvent} from "@testing-library/react"
 
 test("",()=>{
     const componente = render(<h1>Hola Mundo</h1>)

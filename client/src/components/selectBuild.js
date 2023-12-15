160dba9756d95bcbf1e94466ad85d52f8c4959b0
Harkirat <khalsaharkirat97@gmail.com>
2023-12-15 21:59:19 +0100

    slider por terminar

--- client/src/components/selectBuild.js
+++ client/src/components/selectBuild.js
@@ -173,41 +173,146 @@ function SelectBuild({ context }) {
           </div>
 
           {/* Parte derecha */}
-          {/* Parte derecha */}
           <div className="col-md-6">
             <div className="d-flex align-items-center justify-content-center h-100">
               <div className="rounded p-3 w-100 h-100 d-flex flex-column">
                 {/* Contenido para elegir entre dos opciones */}
                 {!mostrarSlider ? (
-                  <div>
-                    <h3>Elige una opción:</h3>
-                    <button
-                      className="btn btn-primary"
-                      onClick={() => handleOpcionSeleccionada("opcion1")}
+                  <div className="flex-grow-1">
+                    <h3 className="verde">Elige una de las dos opciones</h3>
+                    <Button
+                      onClick={() => {
+                        navigate("/PcBuilder");
+                      }}
                     >
-                      Opción 1
-                    </button>
+                      Build from scratch
+                    </Button>
                     <button
                       className="btn btn-secondary"
-                      onClick={() => handleOpcionSeleccionada("opcion2")}
+                      onClick={() => handleOpcionSeleccionada("opcion1")}
                     >
-                      Opción 2
+                      Pre-Made Build
                     </button>
                   </div>
                 ) : (
                   /* Contenido del slider si se elige la Opción 1 */
                   <Slider {...sliderSettings} className="flex-grow-1">
                     <div>
-                      <h3>Slide 1</h3>
-                      <p>Contenido del slide 1</p>
+                      <h1 className="verde">Builds por nichos</h1>
+                      <Row>
+                        <Col xs={3}>
+                          {/* Zona con 3 botones de gamas */}
+                          <Button
+                            onClick={() => {
+                              buildPorNicho("Gamers").then((r) =>
+                                selectBuild(r, false)
+                              );
+                            }}
+                          >
+                            Gamers
+                          </Button>
+                          <Button
+                            onClick={() => {
+                              buildPorNicho("Profesionales").then((r) =>
+                                selectBuild(r, false)
+                              );
+                            }}
+                          >
+                            Profesionales
+                          </Button>
+                          <Button
+                            onClick={() => {
+                              buildPorNicho("Estudiantes").then((r) =>
+                                selectBuild(r, false)
+                              );
+                            }}
+                          >
+                            Estudiantes
+                          </Button>
+                          <Button
+                            onClick={() => {
+                              buildPorNicho("Uso Basico").then((r) =>
+                                selectBuild(r, false)
+                              );
+                            }}
+                          >
+                            Uso Basico
+                          </Button>
+                        </Col>
+                      </Row>
                     </div>
                     <div>
-                      <h3>Slide 2</h3>
-                      <p>Contenido del slide 2</p>
+                      <h1 className="verde">Builds por gama</h1>
+                      <Row>
+                        <Col xs={3}>
+                          {/* Zona con 3 botones de gamas */}
+                          <Button
+                            onClick={() => {
+                              buildPorGama("baja").then((r) =>
+                                selectBuild(r, false)
+                              );
+                            }}
+                          >
+                            Gama Baja{" "}
+                          </Button>
+                          <Button
+                            onClick={() => {
+                              buildPorGama("media").then((r) =>
+                                selectBuild(r, false)
+                              );
+                            }}
+                          >
+                            Gama Media
+                          </Button>
+                          <Button
+                            onClick={() => {
+                              buildPorGama("alta").then((r) =>
+                                selectBuild(r, false)
+                              );
+                            }}
+                          >
+                            Gama Alta
+                          </Button>
+                        </Col>
+                      </Row>
                     </div>
                     <div>
-                      <h3>Slide 3</h3>
-                      <p>Contenido del slide 3</p>
+                      <h3 className="verde">Build por precio</h3>
+                      <Row>
+                        <Col xs={3}>
+                          <Form>
+                            <Form.Label style={{ color: "white" }}>
+                              Presupuesto
+                            </Form.Label>
+                            <Form.Control
+                              type="text"
+                              placeholder="ej: 1000"
+                              value={presupuesto}
+                              onChange={(e) => {
+                                setPresupuesto(e.target.value);
+                              }}
+                            />
+                          </Form>
+                          <Col>
+                            <Button
+                              onClick={() => {
+                                buildPorPrecio(presupuesto).then((result) => {
+                                  if (result && result.Error) {
+                                    // Si buildPorPrecio devuelve un objeto Error, muestra un pop-up con el mensaje de error
+                                    setMensaje(result.Error);
+                                    setShow(true);
+                                  } else {
+                                    // Si no es un error, ejecuta selectBuild con el resultado
+                                    selectBuild(result, secondHand);
+                                  }
+                                });
+                              }}
+                            >
+                              Hacer Build
+                            </Button>
+                          </Col>
+                        </Col>
+                      </Row>
                     </div>
                   </Slider>
                 )}
@@ -216,122 +321,6 @@ function SelectBuild({ context }) {
           </div>
         </div>
       </div>
-      <Container>
-        <h1>Builds por nichos</h1>
-        <Row>
-          <Col xs={3}>
-            {/* Zona con 3 botones de gamas */}
-            <Button
-              onClick={() => {
-                buildPorNicho("Gamers").then((r) => selectBuild(r, false));
-              }}
-            >
-              Gamers
-            </Button>
-            <Button
-              onClick={() => {
-                buildPorNicho("Profesionales").then((r) =>
-                  selectBuild(r, false)
-                );
-              }}
-            >
-              Profesionales
-            </Button>
-            <Button
-              onClick={() => {
-                buildPorNicho("Estudiantes").then((r) => selectBuild(r, false));
-              }}
-            >
-              Estudiantes
-            </Button>
-            <Button
-              onClick={() => {
-                buildPorNicho("Uso Basico").then((r) => selectBuild(r, false));
-              }}
-            >
-              Uso Basico
-            </Button>
-          </Col>
-        </Row>
-        <h1>Builds por gama</h1>
-        <br />
-        <Row>
-          <Col xs={3}>
-            {/* Zona con 3 botones de gamas */}
-            <Button
-              onClick={() => {
-                buildPorGama("baja").then((r) => selectBuild(r, false));
-              }}
-            >
-              Gama Baja{" "}
-            </Button>
-            <Button
-              onClick={() => {
-                buildPorGama("media").then((r) => selectBuild(r, false));
-              }}
-            >
-              Gama Media
-            </Button>
-            <Button
-              onClick={() => {
-                buildPorGama("alta").then((r) => selectBuild(r, false));
-              }}
-            >
-              Gama Alta
-            </Button>
-          </Col>
-        </Row>
-        <br />
-        <h1>Build desde cero</h1>
-        <Row>
-          <Col xs={1}>
-            <div className="main-content">
-              <Button
-                onClick={() => {
-                  navigate("/PcBuilder");
-                }}
-              >
-                Construye tu PC desde cero
-              </Button>
-            </div>
-          </Col>
-        </Row>
-        <br />
-        <h1>Build por precio</h1>
-        <Row>
-          <Col xs={3}>
-            <Form>
-              <Form.Label style={{ color: "white" }}>Presupuesto</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="ej: 1000"
-                value={presupuesto}
-                onChange={(e) => {
-                  setPresupuesto(e.target.value);
-                }}
-              />
-            </Form>
-            <Col>
-              <Button
-                onClick={() => {
-                  buildPorPrecio(presupuesto).then((result) => {
-                    if (result && result.Error) {
-                      // Si buildPorPrecio devuelve un objeto Error, muestra un pop-up con el mensaje de error
-                      setMensaje(result.Error);
-                      setShow(true);
-                    } else {
-                      // Si no es un error, ejecuta selectBuild con el resultado
-                      selectBuild(result, secondHand);
-                    }
-                  });
-                }}
-              >
-                Hacer Build
-              </Button>
-            </Col>
-          </Col>
-        </Row>
-      </Container>
     </div>
   );
 }
